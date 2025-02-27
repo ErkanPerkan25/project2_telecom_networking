713fdd64d49d444aa400e9ebeaa960ebe6ca4d6a
Eric Hansson <erichansson.united@gmail.com>
2025-02-27 09:29:01 -0600

    Working on implementing the encoding for CRC, had an idea but it won't work. I think this is going to be done recursively

--- transmit.cpp
+++ transmit.cpp
@@ -6,10 +6,52 @@
 ************************************************************/
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+string longDivison(string frame, string generator){
+
+    string bits = frame.substr(0,generator.size());
+
+    string extra;
+
+    if(bits[0] == '1' && generator[0] == '1')
+        extra = '1';
+
+    string newBits;
+    for (int i=0; i < bits.size()-1; i++) {
+        if (bits[i+1] == '0' && generator[i+1] == '1') {
+            newBits.push_back('1');
+        } 
+        else if (bits[i+1] == '1' && generator[i+1] == '0') {
+            newBits.push_back('1');
+        } 
+        else if (bits[i+1] == '1' && generator[i+1] == '1') {
+            newBits.push_back('0');
+        } 
+        else if (bits[i+1] == '0' && generator[i+1] == '0') {
+            newBits.push_back('0');
+        } 
+    }
+
+    
+
+    return newBits;
+}
+
 int main(int argc, char *argv[]){
+    string frame, generator;
+    
+    cin >> frame;
+    cin >> generator;
+
+    for(int i=0; i < generator.size()-1; i++)
+        frame.push_back('0');
+
+    
+    
+
 
     return 0;
 }
