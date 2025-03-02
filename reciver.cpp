087767beb2530aaf1528f5891694900dd5cd60f0
Eric Hansson <erichansson.united@gmail.com>
2025-03-01 20:00:06 -0600

    Have the code for the encoding and reciever use the same but it checks if there is a remainder or not, if then ERROR, if no remainder then OK

--- reciver.cpp
+++ reciver.cpp
@@ -4,11 +4,65 @@
 * File:
 * Purpose:
 ************************************************************/
+
 #include <iostream>
 
 using namespace std;
 
+string XOR(string bits1, string bits2){
+    string res = "";
+
+    for(int i=1; i < bits1.size(); i++) {
+        if(bits1[i] == bits2[i]){
+            res.push_back('0');
+        }
+        else {
+            res.push_back('1');
+        }
+    }
+
+    return res;
+}
+
+string longDivison(string frame, string generator){
+
+    string bits = frame.substr(0,generator.size());
+
+    string zeroGen(generator.size(), '0');
+
+
+    for(int i=bits.size(); i <= frame.size(); i++){
+
+        if(bits[0] == '0')
+            bits = XOR(bits, zeroGen);
+        else
+            bits = XOR(bits, generator);
+
+        bits.push_back(frame[i]);
+
+    }
+
+    
+    frame.replace(frame.size()-bits.size()+1, frame.size(), bits);
+
+    cout << bits << endl;
+
+    return frame;
+}
+
+
+
 int main(int argc, char *argv[]){
+    string frame, generator;
+    
+    cin >> frame;
+    cin >> generator;
+
+    for(int i=0; i < generator.size()-1; i++)
+        frame.push_back('0');
+
+    cout << longDivison(frame, generator) << endl;
+
 
     return 0;
 }
--- transmit.cpp
+++ transmit.cpp
@@ -5,40 +5,52 @@
 * Purpose:
 ************************************************************/
 
+#include <functional>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+string XOR(string bits1, string bits2){
+    string res = "";
+
+    for(int i=1; i < bits1.size(); i++) {
+        if(bits1[i] == bits2[i]){
+            res.push_back('0');
+        }
+        else {
+            res.push_back('1');
+        }
+    }
+
+    return res;
+}
+
 string longDivison(string frame, string generator){
 
     string bits = frame.substr(0,generator.size());
 
-    string extra;
+    string zeroGen(generator.size(), '0');
 
-    if(bits[0] == '1' && generator[0] == '1')
-        extra = '1';
 
-    string newBits;
-    for (int i=0; i < bits.size()-1; i++) {
-        if (bits[i+1] == '0' && generator[i+1] == '1') {
-            newBits.push_back('1');
-        } 
-        else if (bits[i+1] == '1' && generator[i+1] == '0') {
-            newBits.push_back('1');
-        } 
-        else if (bits[i+1] == '1' && generator[i+1] == '1') {
-            newBits.push_back('0');
-        } 
-        else if (bits[i+1] == '0' && generator[i+1] == '0') {
-            newBits.push_back('0');
-        } 
+    for(int i=bits.size(); i <= frame.size(); i++){
+
+        if(bits[0] == '0')
+            bits = XOR(bits, zeroGen);
+        else
+            bits = XOR(bits, generator);
+
+        bits.push_back(frame[i]);
+
     }
 
     
+    frame.replace(frame.size()-bits.size()+1, frame.size(), bits);
 
-    return newBits;
+    return frame;
 }
+
+
 
 int main(int argc, char *argv[]){
     string frame, generator;
@@ -49,8 +61,7 @@ int main(int argc, char *argv[]){
     for(int i=0; i < generator.size()-1; i++)
         frame.push_back('0');
 
-    
-    
+    cout << longDivison(frame, generator) << endl;
 
 
     return 0;
